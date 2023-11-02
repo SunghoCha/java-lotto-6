f1c37a2397514b4adeb8b2b3b02ade9b53a482cc
SungHoCha <tjdgh3725@gmail.com>
2023-11-02 18:00:42 +0900

    Feat - "add LottoFactory"
    Lotto를 생성하는 Factory 구현

---
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -0,0 +1,16 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoFactory {
+
+    public static Lotto createWinningLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+    }
+}
---
+++ src/test/java/lotto/domain/LottoFactoryTest.java
@@ -0,0 +1,11 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+public class LottoFactoryTest {
+
+    @Test
+    void Lotto생성() {
+      LottoFactory.createWinningLotto();
+    }
+}
